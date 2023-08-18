0d31639fb35967b9e7a72ef0e5acd45a517e2231
Crooks2k <ciberronaldo@hotmail.com>
2023-08-18 18:08:49 -0500

    feat: array-form unit test

--- src/app/pages/array-form/array-form.component.spec.ts
+++ src/app/pages/array-form/array-form.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ArrayFormComponent } from './array-form.component';
+import { ReactiveFormsModule, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
 
 describe('ArrayFormComponent', () => {
   let component: ArrayFormComponent;
@@ -8,8 +8,9 @@ describe('ArrayFormComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ArrayFormComponent]
-    });
+      declarations: [ArrayFormComponent],
+      imports: [ReactiveFormsModule]
+    }).compileComponents()
     fixture = TestBed.createComponent(ArrayFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +19,23 @@ describe('ArrayFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('AddCardToPay', () =>{
+
+       spyOn(component, 'getCardFields').and.callFake(() => {
+        return new FormGroup({
+          card: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]),
+          cardid: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(4)]),
+        })
+      })
+
+      const initialCardCount = component.cardListArray().length
+
+      component.AddCardToPay()
+
+      const finalCardCount = component.cardListArray().length
+
+      expect(finalCardCount).toBe(initialCardCount + 1)
+  })
 });
+
